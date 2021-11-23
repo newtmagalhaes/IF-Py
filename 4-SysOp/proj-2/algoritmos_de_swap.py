ac190753940a6574cbc43da086ee6ee3d602ea47
newtmagalhaes <aniltonmagalhaes@gmail.com>
2021-11-22 21:18:28 -0300

    Adicionando teste 2 e assinaturas das funções

--- 4-SysOp/proj-2/algoritmos_de_swap.py
+++ 4-SysOp/proj-2/algoritmos_de_swap.py
@@ -1,53 +1,50 @@
 # Implementação de algoritmos de swap
-import numpy as np
-import pandas as pd
 from fila_e_pagina import Fila, Pagina
 
-
 # Algoritmos de Swap
-def fifo(queue:Fila, ref_list:'list[str]') -> int:
+def fifo(f:Fila, ref_list:'list[str]') -> int:
   acerto = 0
   for ref in ref_list:
     id = ref[:-1]
-    if queue.encontrar(id) >= 0:
+    if f.encontrar(id) >= 0:
       acerto += 1
     else:
-      if queue.esta_cheia():
-        queue.desenfilar()
-      queue.enfilar(Pagina(id, 1, 0))
-  print(queue)
+      if f.esta_cheia():
+        f.desenfilar()
+      f.enfilar(Pagina(id, 1, 0))
   return acerto
 
 
-
-def sc():
+def mru(f:Fila, ref_list:'list[str]') -> int:
   pass
 
 
-def mru():
+# Usado nos algoritmos Segunda Chance e NUR
+DELTA_T = 50
+
+
+def sc(f:Fila, ref_list:'list[str]') -> int:
   pass
 
 
-def nur():
+def nur(f:Fila, ref_list:'list[str]') -> int:
   pass
+
 
 if __name__ == '__main__':
+  print('teste 1')
+  ENTRADA = '2W-1R-5R-5W-2R-0W-2R-7R-1W-7W-2R-7W-4R-2R-6W-1W-2W-3W-7R-5R-'
+  REFERENCIAS = ENTRADA.split('-')[:-1] # este slice exclui o ultimo elemento (que seria '')
+  f = Fila(4)
+  acertos = fifo(f, REFERENCIAS)
+  # Fila no final com FIFO é: [1, 3, 7, 5] - 8 acertos
+  print(f'Fila no final da execução: {f}\nacertos: {acertos}')
+
+  print('teste 2')
   # # Pegando input do arquivo.txt
-  # with open('4-SysOp/proj-2/REFERENCIAS_1000.TXT') as file:
-  #   txt = file.read().split('-')[:-1]
-  #   print(len(txt))
-
-  TESTE = '2W-1R-5R-5W-2R-0W-2R-7R-1W-7W-2R-7W-4R-2R-6W-1W-2W-3W-7R-5R-'
-  REFERENCIAS = TESTE.split('-')[:-1] # este slice exclui o ultimo elemento (que seria '')
-  # FRAMES = (3, 4) # (50, 70, 90)
-
-  acertos = fifo(Fila(4), REFERENCIAS)
-  print(f'acertos: {acertos}')
-  # acertos = {}
-  # for alg in [fifo]:
-  #   acertos[alg.__name__] = []
-  #   for n_frames in FRAMES:
-  #     acertos = alg(REFERENCIAS)
-  #     acertos[alg.__name__].append(acertos)
-  
-    
+  with open('4-SysOp/proj-2/REFERENCIAS_1000.TXT') as file:
+    txt = file.read().split('-')[:-1]
+    for n_frames in [50, 70, 90]:
+      for algoritmo in [fifo]:
+        acertos = algoritmo(Fila(n_frames), txt)
+        print(f'{algoritmo.__name__} com {n_frames} frames: {acertos} acertos')
