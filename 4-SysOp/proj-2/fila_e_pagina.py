1450f0468f3ebf9b2c42dd5d0f6a118fcec6e153
newtmagalhaes <aniltonmagalhaes@gmail.com>
2021-11-22 21:11:01 -0300

    Adicionando fila e página

--- 4-SysOp/proj-2/fila_e_pagina.py
+++ 4-SysOp/proj-2/fila_e_pagina.py
@@ -1,62 +1,63 @@
-# Tipos de Fila e Páginas usados
+# Tipos de Fila e Páginas usados no projeto
 from dataclasses import dataclass
 
 @dataclass
 class Pagina:
   id:str
-  R:bool
-  M:bool
-
-@dataclass
-class _Node:
-  pagina:Pagina
-  next: '_Node|None' = None
+  R:bool = 0
+  M:bool = 0
 
 class Fila:
   def __init__(self, limite:int) -> None:
-    self.limite = limite
-    self.capacidade = 0
-    self.head : '_Node|None' = None
+    '''
+    Imita o comportamento de uma Fila de Páginas
+    '''
+    self.LIMITE = limite
+    self.frames_ocupados = 0
+    self.pag_list:'list[Pagina]' = []
   
   def __str__(self) -> str:
     s = '['
-    node = self.head
-    while node is not None:
-      s += f'{node.pagina.id} - '
-      node = node.next
-    return s + ']'
+    for pag in self.pag_list:
+      s += f' {pag.id} -'
+    return s[:-1] + ']'
 
   def esta_vazia(self) -> bool:
-    return self.capacidade == 0
+    return self.frames_ocupados == 0
   
   def esta_cheia(self) -> bool:
-    return self.capacidade == self.limite
+    return self.frames_ocupados == self.LIMITE
   
-  def encontrar(self, id:str) -> bool:
-    i = 0
-    node = self.head
-    while node is not None:
-      if node.pagina.id == id:
+  def encontrar(self, id:str) -> int:
+    '''
+    Verifica se a fila possui uma página com o `id` especificado.
+    - Se tiver: retorna a posição da página na fila;
+    - Senão: retorna -1; 
+    '''
+    for i, pag in enumerate(self.pag_list):
+      if pag.id == id:
         return i
-      i += 1
-      node = node.next
     return -1
 
   def enfilar(self, pag:Pagina) -> None:
-    if self.head is None:
-      self.head = _Node(pag)
-    else:
-      node = self.head
-      while node.next is not None:
-        node = node.next
-      node.next = _Node(pag)
-    self.capacidade += 1
+    '''
+    Adiciona a página no fim da fila se a mesma
+    não estiver cheia.
+    '''
+    if not self.esta_cheia():
+      self.pag_list.append(pag)
+      self.frames_ocupados += 1
     
 
-  def desenfilar(self) -> Pagina:
-    out = self.head
-    if out is not None:
-      self.head = self.head.next
-      out.next = None
-    self.capacidade -= 1
-    return out
+  def desenfilar(self, pos : int = 0) -> 'Pagina|None':
+    '''
+    Remove o elemento da posição `pos` da fila e o retorna;
+    
+    Retorna `None` se a fila estiver vazia;
+    
+    Gera erro se o indice `pos` exceder o tamanho da fila.
+    '''
+    if not self.esta_vazia():
+      out = self.pag_list.pop(pos)
+      self.frames_ocupados -= 1
+      return out
